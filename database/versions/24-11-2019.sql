0db7907440a2cdd602df477defddf88f577bac05
Ayman Hussein <aymaan.hussein@gmail.com>
2019-11-24 16:34:01 +0200

    update

--- database/versions/24-11-2019.sql
+++ database/versions/24-11-2019.sql
@@ -1,1 +1,2 @@
 ﻿INSERT INTO `permissions` (`id`, `name`, `label`, `is_menu`, `order_list`, `icon`, `parent_id`, `created_at`, `updated_at`) VALUES (NULL, 'pricing', 'عرض الاسعار', '1', '05', 'icon-book-open', '0', CURRENT_TIMESTAMP, NULL);
+INSERT INTO `permission_role` (`id`, `role_id`, `permission_id`, `created_at`, `updated_at`) VALUES (NULL, '1', '232', CURRENT_TIMESTAMP, NULL);
