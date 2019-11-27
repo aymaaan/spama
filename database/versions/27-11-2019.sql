be739006a75ab9b5a3c681556717db05bce91b81
Ayman Hussein <aymaan.hussein@gmail.com>
2019-11-27 18:59:50 +0200

    update sql

--- database/versions/27-11-2019.sql
+++ database/versions/27-11-2019.sql
@@ -1,1 +1,10 @@
 ALTER TABLE `customers_assessment_products` ADD `unit_price` VARCHAR(255) NULL AFTER `unit_id`;
+
+
+DROP TABLE IF EXISTS `customers_pricing_settings`;
+CREATE TABLE IF NOT EXISTS `customers_pricing_settings` (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `serial` int(11) NOT NULL,
+  `discount` varchar(11) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
