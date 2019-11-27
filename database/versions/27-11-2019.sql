15cb80b284a4959c3b8deede421b1eec88b801ea
Ayman Hussein <aymaan.hussein@gmail.com>
2019-11-27 20:02:27 +0200

    update sql

--- database/versions/27-11-2019.sql
+++ database/versions/27-11-2019.sql
@@ -8,3 +8,6 @@ CREATE TABLE IF NOT EXISTS `customers_pricing_settings` (
   `discount` varchar(11) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+ALTER TABLE `customers_pricing_settings` ADD `created_at` TIMESTAMP NULL AFTER `discount`, ADD `updated_at` TIMESTAMP NULL AFTER `created_at`;
+
