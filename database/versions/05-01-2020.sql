a2a2d7a21c37d8f903cd30efaeea68092da71adf
Ayman Hussein <aymaan.hussein@gmail.com>
2020-01-05 15:51:08 +0200

    update sql

--- database/versions/05-01-2020.sql
+++ database/versions/05-01-2020.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS `departments`;
+﻿DROP TABLE IF EXISTS `departments`;
 CREATE TABLE IF NOT EXISTS `departments` (
   `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `title` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
@@ -9,3 +9,10 @@ CREATE TABLE IF NOT EXISTS `departments` (
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+INSERT INTO `permissions` (`id`, `name`, `label`, `is_menu`, `order_list`, `icon`, `parent_id`, `created_at`, `updated_at`) VALUES (NULL, 'departments', 'الاقسام الادارية', '1', '07', 'la la-group', '0', CURRENT_TIMESTAMP, NULL);
+INSERT INTO `permissions` (`id`, `name`, `label`, `is_menu`, `order_list`, `icon`, `parent_id`, `created_at`, `updated_at`) VALUES (NULL, 'create_departments', 'اضافة قسم ادارى', '0', '', NULL, '233', CURRENT_TIMESTAMP, NULL);
+INSERT INTO `permissions` (`id`, `name`, `label`, `is_menu`, `order_list`, `icon`, `parent_id`, `created_at`, `updated_at`) VALUES (NULL, 'update_departments', 'تحديث قسم ادارى', '0', NULL, NULL, '233', CURRENT_TIMESTAMP, NULL);
+INSERT INTO `permissions` (`id`, `name`, `label`, `is_menu`, `order_list`, `icon`, `parent_id`, `created_at`, `updated_at`) VALUES (NULL, 'delete_departments', 'حذف قسم ادارى', '0', NULL, NULL, '233', CURRENT_TIMESTAMP, NULL);
+
+
