25cf0a52923b8bae28b3199892710934d84afe1d
Ayman Hussein <aymaan.hussein@gmail.com>
2020-01-18 11:14:14 +0200

    delete old database

--- database/versions/15-01-2020.sql
+++
@@ -1,1 +0,0 @@
-UPDATE `permissions` SET `is_menu` = '0' WHERE `permissions`.`id` = 212;
--- database/versions/28-11-2019.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE `customers_assessment_products` ADD `discount` VARCHAR(255) NULL AFTER `user_id`;
